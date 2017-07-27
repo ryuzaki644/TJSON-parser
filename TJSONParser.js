6fd762af2b11182e4fc97ec39681f9684a8185f4
ryuzaki644 <sathwikkoushal@gmail.com>
2017-07-27 15:35:25 +0530

    Add arrayParser, objectParser and parserFactory

--- TJSONParser.js
+++ TJSONParser.js
@@ -1,3 +1,5 @@
+const inp = require('fs').readFileSync('./example.json').toString()
+
 const stringParser = input => {
   input = input.trim() // change to regex later
   if (input.startsWith('"') === false) return null
@@ -25,7 +27,6 @@ const tagParser = input => {
   return [tagRegex.exec(input)[1], tagRegex.exec(input)[2]]
 }
 
-
 const colonParser = input => {
   input = input.trim()
   if (input.startsWith(':') === false) return null
@@ -39,3 +40,74 @@ const commaParser = (input) => {
   }
   return null
 }
+
+const parserFactory = (tag) => {
+  if (tag === 's') return stringParser
+  if (tag === 'i') return intParser
+  if (tag === 'O') return objectParser
+
+  if (tag.startsWith('A') === true) {
+    return (tag) => {
+      const insideTag = 's'
+      return arrayParserGenerator(insideTag)
+    }
+  }
+  return input => null
+}
+const arrayParserGenerator = (insideTag) => input => {
+  let tempArray = []
+  let requiredOut, comma
+  input = input.trim()
+  if (input.startsWith('[') === false) return null
+  input = input.slice(1)
+  input = input.trim()
+  if (insideTag === '' && input.startsWith(']')) return [[], input.slice(1)]
+  let requiredParser = parserFactory(insideTag)
+  while (input !== null) {
+    input = input.trim()
+    requiredOut = requiredParser(input)
+    tempArray.push(requiredOut[0])
+    input = requiredOut[1]
+    comma = commaParser(requiredOut[1])
+    if (comma === null) break
+    input = comma[1]
+  }
+  input = input.trim()
+  if (input.startsWith(']') === true) return [tempArray, input.slice(1)]
+  return null
+}
+
+const objectParser = input => {
+  if (input.startsWith('{') === false) return null
+
+  let obj = {}
+  let key, value, tag, comma
+  input = input.slice(1)
+  while (input !== null) {
+    key = keyParser(input)[0]
+    tag = tagParser(keyParser(input)[1])[0]
+    value = tagParser(keyParser(input)[1])[1]
+    value = value.trim()
+    if (value.startsWith('"') === false) return null
+    value = value.slice(1)
+    value = colonParser(value)
+    if (value === null) return null
+    value = value[1]
+    let reqParser = parserFactory(tag)
+    value = reqParser()(value)
+    if (value === null) return null
+    obj[key] = value[0]
+    input = value[1]
+    comma = commaParser(input)
+    if (comma === null) break
+    input = comma[1]
+  }
+  input = input.trim()
+  if (input.startsWith('}') === true) return [obj, input.slice(1)]
+  return null
+}
+
+const out = objectParser(inp)
+if (out === null) {
+  console.log('INVALID TJSON')
+} else console.log(JSON.stringify(out[0], null, 2))
